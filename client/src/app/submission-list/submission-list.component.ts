7d659260c75521d41b8dfaa22b6f63e5a548c4fb
handtravis <travishand02@gmail.com>
2025-07-22 07:29:04 -0700

    creating angular component to display g split submission

---
+++ client/src/app/submission-list/submission-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, WritableSignal } from '@angular/core';
+import { Submission } from '../submission'
+import { SubmissionService } from '../submission.service';
+import { RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-submission-list',
+  imports: [],
+  template: `
+    <p>
+      submission-list works!
+    </p>
+  `,
+  styles: ``
+})
+export class SubmissionListComponent {
+
+}
