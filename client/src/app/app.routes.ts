4be1943d005fbc950dc8c8966339d6019107fa4f
handtravis <travishand02@gmail.com>
2025-07-23 14:07:25 -0700

    updated routes to reflect new SPA layout

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SubmissionListComponent } from './submission-list/submission-list.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { SubmissionHistoryComponent } from './submission-history/submission-history.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomepageComponent, title: 'Home Page'},
+    { path: 'history', component: SubmissionHistoryComponent}
+];
--- client/src/app/submission-list/submission-list.component.ts
+++ client/src/app/submission-list/submission-list.component.ts
@@ -2,17 +2,26 @@ import { Component, OnInit, WritableSignal } from '@angular/core';
 import { Submission } from '../submission'
 import { SubmissionService } from '../submission.service';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 
 @Component({
   selector: 'app-submission-list',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule, MatButtonModule, MatCardModule],
+  styles: [
+    `
+      button:first-of-type {
+        margin-right: 1rem;'
+      }
+    `
+  ],
   template: `
     <p>
       submission-list works!
     </p>
-  `,
-  styles: ``
+  `
 })
 export class SubmissionListComponent {
 
